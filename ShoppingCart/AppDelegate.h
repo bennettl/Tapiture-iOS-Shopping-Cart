2490571d3c5f70d119d34040e1e31fa117264020
Bennett Lee <bennett@tapure.com>
2014-07-11 12:10:15 -0700

    Initial Commit

---
+++ ShoppingCart/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShoppingCart
+//
+//  Created by Bennett Lee on 7/11/14.
+//  Copyright (c) 2014 Bennett Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShoppingCart/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShoppingCart
+//
+//  Created by Bennett Lee on 7/11/14.
+//  Copyright (c) 2014 Bennett Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
